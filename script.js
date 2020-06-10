90aeb7323bb9e52d830d0a2113eaa976638c8db9
Mac Tung <mactrantung@gmail.com>
2020-06-10 23:40:56 +0700

    delete file

---
+++ script.js
@@ -0,0 +1,34 @@
+var canvas = document.getElementById('canvas');
+console.log(window.innerWidth);
+var ctx = canvas.getContext('2d');
+let cw = canvas.width;
+let ch = canvas.height;
+let fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', function () {
+    const FR = new FileReader();
+    FR.addEventListener('load', (evt) => {
+        const img = new Image();
+        img.src = evt.target.result;
+        img.onload = function () {
+            canvas.width = window.innerWidth;
+            canvas.height = img.height * (window.innerWidth / img.width);
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            drawLogo();
+        };
+    });
+    FR.readAsDataURL(this.files[0]);
+});
+
+function drawLogo() {
+    let logoImg = new Image();
+    logoImg.src = './logo.png';
+    logoImg.onload = function () {
+        ctx.drawImage(
+            logoImg,
+            20,
+            20,
+            canvas.width / 3.5,
+            logoImg.height * (canvas.width / 3.5 / logoImg.width)
+        );
+    };
+}
